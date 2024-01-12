55b1e464edd14e2a4869f032312853d7b0c8a6eb
SiewJun <siewkhaijun57@gmail.com>
2024-01-13 02:42:19 +0800

    Initial commit

---
+++ app/Models/Payment.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Payment extends Model
+{
+  use HasFactory;
+
+  protected $fillable = [
+    'cart_id',
+    'payment_method',
+  ];
+
+  public function cart()
+  {
+    return $this->belongsTo(Cart::class);
+  }
+
+  public function carts()
+  {
+    return $this->belongsToMany(Cart::class, 'payment_cart');
+  }
+}
---
+++ database/migrations/2024_01_08_094424_create_bookings_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained(); // Foreign key to relate the booking to a user
+            $table->foreignId('cart_id')->constrained(); // Foreign key to relate the booking to a cart
+            $table->date('booking_date'); // Date when the booking was made
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('bookings');
+    }
+};
